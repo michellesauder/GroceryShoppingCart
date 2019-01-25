f2c38ea8ce5839ed6f68aab23944d9b9ef2006f7
Michelle Sauder <mgsauder@hotmail.com>
2019-01-25 11:05:03 -0800

    Updated Search functionality

--- GroceryShoppingCart/Repositories/DrinksRepo.cs
+++ GroceryShoppingCart/Repositories/DrinksRepo.cs
@@ -17,20 +17,22 @@ namespace GroceryShoppingCart.Repositories
             this.db = db;
         }
 
-        public IQueryable<Drinks> GetAll(string sortOrder, string searchString)
+        public IEnumerable<Drinks> GetAll(string sortOrder, string searchString)
         {
-            IQueryable<Drinks> drinkSearch;
+            IEnumerable<Drinks> drinkSearchSort;
+
             if (!String.IsNullOrEmpty(searchString))
             {
-                drinkSearch = db.Drinks.Select(d => new Drinks
+                drinkSearchSort = db.Drinks.Select(d => new Drinks
                 {
+                    //DrinkId = d.GroceryCart.Select(di => di.DrinkId),
                     Description = d.Description,
                 })
                     .Where(d => d.Description.Contains(searchString));
             }
             else
             {
-                drinkSearch = db.Drinks.Select(d => new Drinks
+                drinkSearchSort = db.Drinks.Select(d => new Drinks
                 {
                     Description = d.Description
 
@@ -38,33 +40,37 @@ namespace GroceryShoppingCart.Repositories
             }
 
 
-            var DrinkSort =
-            db.Drinks.Select(d => new Drinks
-            {
-                Description = d.Description
-            }
-            );
+            //var DrinkSort =
+            //db.Drinks.Select(d => new Drinks
+            //{
+            //    Description = d.Description
+            //}
+            //);
 
             switch (sortOrder)
             {
                 case "description_asc":
-                    DrinkSort =
-                        DrinkSort.OrderBy(d => d.Description);
+                    drinkSearchSort =
+                        drinkSearchSort.OrderBy(d => d.Description);
                     break;
                 case "title_desc":
-                    DrinkSort =
-                        DrinkSort.OrderByDescending(d => d.Description);
+                    drinkSearchSort =
+                        drinkSearchSort.OrderByDescending(d => d.Description);
                     break;
                 case "description_desc":
-                    DrinkSort =
-                  DrinkSort.OrderByDescending(d => d.Description);
+                    drinkSearchSort =
+                  drinkSearchSort.OrderByDescending(d => d.Description);
                     break;
                 default:
-                    DrinkSort =
-                        DrinkSort.OrderBy(d => d.Description);
+                    drinkSearchSort =
+                        drinkSearchSort.OrderBy(d => d.Description);
                     break;
             };
-            return DrinkSort;
+            return drinkSearchSort;
+
+
+
+
         }
     }
 }
